695a9cb1c88c1757f7532fddde76cc5aa6de3d3b
vishal kumar <vishakumar041200@gmail.com>
2025-02-09 01:38:46 +0530

    done

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                 return (
                     <div className="block max-w-sm p-6 bg-white border cursor-pointer border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" key={index}>
 
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{fact.country}</h5>
+                    <h5 className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{fact.country}</h5>
                     <span className='font-bold '>Capital:</span> <span className='text-gray-200'>{fact.capital}</span><br />
                     <span className='font-bold '>Population:</span> <span className='text-gray-200'>{fact.population}</span><br />
                     <span className='font-bold '>interestingFact:</span> <span className='text-gray-200'>{fact.interestingFact}</span><br />
